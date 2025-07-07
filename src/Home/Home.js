d547bc136c81f52086cea9e8afce06107f715b82
josh-umahi <jaayaustin@gmail.com>
2025-07-07 09:19:38 +0100

    Updated to question range 1 to 1000

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -5,40 +5,40 @@ import './home.css';
 import acsa_logo from "../assets/acsa_logo.png"
 
 const Home = () => {
-    const [number_input_entered, set_number_input_entered] = useState(null)
-    const navigate = useNavigate();
+  const [number_input_entered, set_number_input_entered] = useState(null)
+  const navigate = useNavigate();
 
-    const changeQuestion = (question_number_parameter) => {
-        const question_number = parseInt(question_number_parameter.trim())
-        if (isNaN(question_number) || !Number.isInteger(question_number) || question_number < 1 || question_number > 200) {
-            return
-        }
-
-        navigate(`/roundOfGame/${question_number}`);
+  const changeQuestion = (question_number_parameter) => {
+    const question_number = parseInt(question_number_parameter.trim())
+    if (isNaN(question_number) || !Number.isInteger(question_number) || question_number < 1 || question_number > 1000) {
+      return
     }
 
-    return (
-        <div className='homeSection'>
-            <header>
-                <img className='acsaImg' src={acsa_logo} alt="logo" />
-                <h1 className='textLogo'>The ACSA Family Feud</h1>
-                <form className='input_div' onSubmit={(e) => {
-                    e.preventDefault()
-                    changeQuestion(number_input_entered)
-                }}>
-                    <div>
-                        <input onChange={e => set_number_input_entered(e.target.value)} type="text" placeholder='Enter a number' id="" />
-                        <button type="submit">submit</button>
-                    </div>
-                    <p>Enter a number between 1 and 200</p>
-                </form>
-            </header>
-            <footer>
-                <h4>Created by Yours Truly.</h4>
-                <h4>Yours Truly is me, Joshua Umahi.</h4>
-            </footer>
-        </div>
-    )
+    navigate(`/roundOfGame/${question_number}`);
+  }
+
+  return (
+    <div className='homeSection'>
+      <header>
+        <img className='acsaImg' src={acsa_logo} alt="logo" />
+        <h1 className='textLogo'>The ACSA Family Feud</h1>
+        <form className='input_div' onSubmit={(e) => {
+          e.preventDefault()
+          changeQuestion(number_input_entered)
+        }}>
+          <div>
+            <input onChange={e => set_number_input_entered(e.target.value)} type="text" placeholder='Enter a number' id="" />
+            <button type="submit">submit</button>
+          </div>
+          <p>Enter a number between 1 and 1000</p>
+        </form>
+      </header>
+      <footer>
+        <h4>Created by Yours Truly.</h4>
+        <h4>Yours Truly is me, Joshua Umahi.</h4>
+      </footer>
+    </div>
+  )
 }
 
 export default Home
